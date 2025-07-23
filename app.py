160922e649e6a030d0d641fd0e82b768f57eb05f
Zahra Poorshamsi <zahrahussain0313@gmail.com>
2025-07-23 16:38:21 +0400

    Changed Flask port to 8080 for Cloud Run deployment

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ if __name__ == "__main__":
     local_ip = socket.gethostbyname(hostname)
     print("Flask server is starting...")
     print(f"Flask server running at: http://{local_ip}:5000")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
 
